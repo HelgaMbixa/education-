055348671d4adb2ca9db97d27a5ca4ded6e32c26
HelgaMbixa <69455376+HelgaMbixa@users.noreply.github.com>
2020-09-13 18:34:08 +0300

    13-09-2020
    1- ключевое слово вводится по буквам;
    2- добавлены ключевые слова;
    3- искомый сайт открывается в той же вкладке;
    4- сделали временной интервал между действиями скрипта;
    5- скрипт может работать на искомом сайте;
    6- скрипт ищет сайт на разных страницах поисковика до 10-й страницы включительно;
    7- скрипт переходит по случайным ссылкам на искомом сайте;
    8- скрипт возвращается на сайт-поисковик с вероятностью 15%;

--- bot_for_yandex.js
+++ bot_for_yandex.js
@@ -5,24 +5,52 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://yandex.ru/*
+// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @grant        none
 // ==/UserScript==
 
-let keywords = ['гобой', 'как звучит флейта', 'кларнет'];
+let yandexInput = document.getElementById("text");
+let keywords = ['гобой', 'как звучит флейта', 'кларнет','скрипка', 'саксофон', ',балабан', 'валторна', 'корнет', 'сузафон', 'труба', 'тромбон', 'флюгельгорн',];
 let keyword = keywords[getRandom(0,keywords.length)];
 let button = document.getElementsByClassName("button_theme_websearch")[0];
-if(button != undefined){
-    document.getElementById("text").value= keyword;
-    document.getElementsByClassName("button_theme_websearch")[0].click();
-}else{
-    let links = document.links;
+let i=0;
+let links = document.links;
 
+if(button != undefined){
+    let timerId = setInterval(()=>{
+    yandexInput.value += keyword[i];
+    i++;
+    if(i==keyword.length){
+        clearInterval(timerId);
+        button.click();
+    }
+},1000);
+}else if(location.hostname == "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai"){
+    setInterval(()=>{
+        let index = getRandom(0,links.length);
+        if (getRandom(0,101)>=85){
+            location.href = 'https://yandex.ru';
+        }
+        else if (links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1)  
+            links[index].click();
+    },getRandom(3000,7000));
+}else{
+    let nextPage = true;
     for(let i=0; i<links.length; i++){
         if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
-            console.log("Ссылка найдена"+links[i]);
-            links[i].click();
+            let link = links[i];
+            nextPage = false;
+            link.removeAttribute('target');
+            setTimeout(()=>{link.click();}, getRandom(1000,4000));
             break;
         }
+    }
+    if(document.querySelector('span.pager__item_kind_page').innerText==10){
+        nextPage = false;
+        location.href = 'https://yandex.ru';
+    }
+    if (nextPage){
+    setTimeout(()=>{document.getElementsByClassName("pager__item_kind_next")[0].click();}, getRandom(1000,8000));
     }
 }
 function getRandom(min, max){
